964eed10f0882702b3e02770f2505915f4a1021d
charlizekitto-beep <charlize.kitto@education.nsw.gov.au>
2026-07-02 09:19:44 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,69 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . 4 4 4 4 4 2 2 2 
+. . . . . . . . 4 4 4 4 2 4 2 2 
+. . . . . . . . . 4 4 4 2 4 4 4 
+. . . . . . . . . 4 4 4 4 4 4 4 
+. . . . . . . . . . 4 4 4 4 4 4 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+a 9 9 8 e e e 7 5 5 4 e e e 2 2 
+a a 9 f f f f f 5 5 e f f f e e 
+a a 9 f 8 8 7 f 5 e f 5 4 4 f e 
+a a 9 f f 8 f f 7 e f f 4 f f e 
+a a a f 9 f 8 f 7 e f 5 f 4 f e 
+a a a f f f f f 8 e f f f f f e 
+c a f 9 9 f 9 8 8 f e e f e e f 
+c c a f 9 f 9 f f 7 f e f e f 5 
+c c a a f f f 8 8 8 7 f f f 5 5 
+3 c a a a f 9 8 8 8 8 7 f 7 5 5 
+3 3 c a a f 9 9 8 8 8 8 f 7 7 7 
+3 3 c a a f 9 9 9 9 8 8 f 7 7 7 
+3 3 c f f f f f 9 9 f f f f f 8 
+3 3 3 c c c c a a a a 9 9 8 8 8 
+3 3 3 3 c c c a a a a 9 9 9 9 9 
+3 3 3 3 3 3 3 c c c c c c c c c 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
